e6c1ac9472c6263523b1527214507fc9a4cf67e9
alexgorji <aligorji@hotmail.com>
2024-04-20 15:44:19 +0200

    create_backup changes

--- create_backup.sh
+++ create_backup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-echo "backend: copying files from protected to backup/ "
-mkdir -p /var/www/app/backup-data/backend/
+echo "backend: creating backup-data/backend if not exists"
+mkdir -p /var/www/app/backup-data/backend
+
+echo "backend: cleaning backup-data/backend"
+rm -rf /var/www/app/backup-data/backend/*
+
+echo "backend: copying files from protected to backup-data/backup/ "
 cp -r /var/www/app/protected /var/www/app/backup-data/backend/
