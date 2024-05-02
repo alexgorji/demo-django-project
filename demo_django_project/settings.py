9e2e5fd56b5cc963cceb40797babc23afa7d65b7
alexgorji <aligorji@hotmail.com>
2024-05-02 15:37:00 +0200

    env var DJANGO_ALLOWED_HOSTS renamed to ALLOWED_HOSTS

--- demo_django_project/settings.py
+++ demo_django_project/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 # Application definition
 
